c5c4288d64d26cd40809f0b496ddc631dc61912f
ael-amin <Root-07>
2022-12-14 10:20:30 -0800

    add solution

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * print_alphabet_x10 that print 10 times the alphabet in lowercase.
+ * print_alphabet_x10 - that print 10 times the alphabet in lowercase.
  * Return : 0
  */
 void print_alphabet_x10(void)
