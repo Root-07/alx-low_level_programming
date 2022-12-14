4326b3f23369c00b5b8f99b982e36980c42cb1ec
ael-amin <Root-07>
2022-12-14 08:01:51 -0800

    add solution

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/* Fun that prints the alphabet, in lowercase
+ * use _putchar twice in my code
+ */
+void	print_alphabet(void)
+{
+	char c = 'a';
+
+	while ( c <= 'z')
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar('\n');
+}
+
