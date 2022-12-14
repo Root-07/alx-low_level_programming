d85b3a97ee6fcc9c46d8cf1e18de64a1adb28a66
ael-amin <Root-07>
2022-12-14 10:14:14 -0800

    add solution

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/** print_alphabet_x10 that print 10 times the alphabet in lowercase.
+ * Return : 0
+ */
+void print_alphabet_x10(void)
+{
+	char c;
+	int i = 0;
+
+	while (i < 10)
+	{
+		c = 'a';
+		while (c <= 'z')
+		{
+			_putchar(c);
+			c++;
+		}
+		_putchar('\n');
+	 	i++;
+	}
+}
+
