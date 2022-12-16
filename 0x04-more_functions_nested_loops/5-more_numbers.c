6a2165bf62f13bd75ac011feb6bc7052b9164fd8
ael-amin <Root-07>
2022-12-16 15:04:58 -0800

    add 0x04. C - More functions, more nested loops

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,27 +1,26 @@
 #include "main.h"
 
 /**
- * more_numbers - Print numbers between 0 to 14 incl.
- *
- * Return: Void.
+ * more_numbers - print 10 times the numbers from 0 to 14
+ * Description: Can only use _putchar 3 times
  */
 void more_numbers(void)
 {
-	int i;
-	int x;
+	int c, i;
 
-	for (x = 0; x < 10; x++)
+	c = 0;
+	while (c < 10)
 	{
 		i = 0;
-		while (i <= 14)
+		while (i < 15)
 		{
-			if (i >= 10)
-			{
+			if (i > 9)
 				_putchar(i / 10 + '0');
-			}
-				_putchar(i % 10 + '0');
+
+			_putchar(i % 10 + '0');
 			i++;
 		}
-	_putchar('\n');
+		c++;
+		_putchar('\n');
 	}
 }
