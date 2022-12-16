581e59a0d099f96d76c43947f30f442b420db219
ael-amin <Root-07>
2022-12-16 09:11:24 -0800

    0x03. C - Debugging Folder - Added

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,6 +1,7 @@
 #include "main.h"
+
 /**
-* main - tests function that prints if integer is positive or negative
+* 0-main - tests function that prints if integer is positive or negative
 * Return: 0
 */
 
